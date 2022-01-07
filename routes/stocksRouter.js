7715dd87068a4c4470844fc973f7a0c379b47d2d
Dakota Korn <DakotaJKorn@gmail.com>
2022-01-07 07:28:34 -0500

    Cleaned up code in the /stocks/stockArray and /stocks/:stock_symbol routes

--- routes/stocksRouter.js
+++ routes/stocksRouter.js
@@ -21,47 +21,31 @@ router.get('/', (request, response) => {
          else         
             response.status(200).send(data.Items); 
     });
-    
+
 });
 
-// access all the customers in the table
+// access a group of stocks
 router.get('/stockArray/:stock_symbols_separated_by_commas', (request, response) => {
+
     const array_of_stock_symbols = request.params["stock_symbols_separated_by_commas"].toUpperCase().split(",");
 
     let stocks = {
         "stock_array":[],
         "errors":[]
     }
-
-
-    let params = {
-        TableName: "Stocks",
-        Key:{
-                "stock_symbol": ""
-        }
-    };
     
-
     for(let stock_symbol of array_of_stock_symbols){
-
-        params.Key.stock_symbol = stock_symbol;
         
-        docClient.get(params, function(err,data){
-            if(err){
-                    console.log("stock::get::error - " + JSON.stringify(err,null,2));
-                    
-            }
+        docClient.get({ TableName: "Stocks", Key:{ "stock_symbol": stock_symbol } }, function(err,data){
+            if(err)
+                    console.log("stock::get::error - " + JSON.stringify(err,null,2)); 
             else{
-                    console.log("stock::get::success");
-                    if(data.Item != null){
+                    if(data.Item != null)
                         stocks.stock_array.push(data.Item);
-                    }
-                    else{
-                        stocks.errors.push("Error: Couldn't find stock with symbol: " + stock_symbol);
-                    }
-                    
+                    else
+                        stocks.errors.push("Error: Couldn't find stock with symbol: " + stock_symbol);   
             }
-    })
+        })
 
     }
 
@@ -72,34 +56,20 @@ router.get('/stockArray/:stock_symbols_separated_by_commas', (request, response)
 });
 
 
-
+// access one stock in the database
 router.get('/:stock_symbol', (request, response) => {
 
-    let stock;
-
-    let params = {
-        TableName: "Stocks",
-        Key:{
-                "stock_symbol": ""
-        }
-    };
-
-    params.Key.stock_symbol = request.params["stock_symbol"].toUpperCase();
-
-    docClient.get(params, function(err,data){
+    docClient.get({ TableName: "Stocks", Key: {"stock_symbol": request.params["stock_symbol"].toUpperCase()} }, function(err,data){
         if(err){
                 console.log("stock::get::error - " + JSON.stringify(err,null,2));
         }
         else{
-                //console.log("stock::get::success");
-                stock = data.Item;
-
-                if(stock == null)
-                response.status(200).send("Error: Stock couldn't be found in database.");
+                if(data.Item == null)
+                    response.status(200).send("Error: Stock couldn't be found in database.");
                 else
-                response.status(200).send(stock);
+                    response.status(200).send(data.Item);
         }
-})
+    })
 });
 
 
