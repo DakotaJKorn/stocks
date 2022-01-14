93e205ea447a6a96f9c3f6f49529bdf22e590b50
DakotaJKorn <DakotaJKorn@gmail.com>
2022-01-14 15:29:50 -0500

    Delete config directory

--- config/dbConfig.js
+++
@@ -1,8 +0,0 @@
- // store the configuration secrets of the PG DB here
- module.exports = {
-    HOST: 'stocks-archive-database.cfc6zidigumf.us-east-1.rds.amazonaws.com', // your endpoint
-    USER: 'postgres_Dakota', // your username
-    PASSWORD: 'postgres_Korn', // your password
-    DB: 'stocks_archives_db', // your db name
-    dialect: 'postgres',
-    }
